b6cf6571cc74bfece9069a2f353749d7da2278fd
Iwan Timmer <irtimmer@gmail.com>
2023-04-06 18:09:27 +0200

    server: run rules using execute to reset watchers

--- server/rules.ts
+++ server/rules.ts
@@ -14,14 +14,14 @@ export default class Rules {
         this.providers = providers
 
         providers.on("register", (service: Service) => {
-            this.#rules.filter(r => r.watchServices.includes(service.uniqueId)).forEach(r => r.run())
+            this.#rules.filter(r => r.watchServices.includes(service.uniqueId)).forEach(r => r.execute())
         })
 
         providers.on("update", (service: Service, key: string) => {
-            this.#rules.filter(r => r.watchProperties.includes(`${service.uniqueId}/${key}`)).forEach(r => r.run())
+            this.#rules.filter(r => r.watchProperties.includes(`${service.uniqueId}/${key}`)).forEach(r => r.execute())
         })
 
         config.forEach((r) => this.#rules.push(new JSRule(r, this)))
-        setImmediate(() => this.#rules.forEach(r => r.run()))
+        setImmediate(() => this.#rules.forEach(r => r.execute()))
     }
 }
