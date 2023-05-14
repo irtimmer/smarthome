30c086e2b595e8754ae267c8bb166404714a03d4
Iwan Timmer <irtimmer@gmail.com>
2023-05-14 11:57:09 +0200

    web: show edit popup on click for editable string properties

--- web/components/PropertyInput.vue
+++ web/components/PropertyInput.vue
@@ -2,6 +2,12 @@
   <PropertyValue v-if="property.read_only" :value="modelValue" :property="property" />
   <q-input v-else-if="property.type == 'number'" :modelValue="modelValue" borderless :min="property.min" :max="property.max" type="number" dense />
   <q-toggle v-else-if="property.type == 'boolean'" :modelValue="modelValue" @update:modelValue="$emit('update:modelValue', $event)"></q-toggle>
+  <span v-else-if="property.type == 'string'">
+    <PropertyValue :value="modelValue" :property="property" />
+    <q-popup-edit :modelValue="modelValue" @update:modelValue="$emit('update:modelValue', $event)" :title="property.label" auto-save v-slot="scope">
+      <q-input v-model="scope.value" dense autofocus @keyup.enter="scope.set" />
+    </q-popup-edit>
+  </span>
   <PropertyValue v-else :value="modelValue" :property="property" />
 </template>
 
