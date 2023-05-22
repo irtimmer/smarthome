1d7b3b3e218551758770a26d91ccebb9e0fe33b2
Iwan Timmer <irtimmer@gmail.com>
2023-05-22 17:02:04 +0200

    web: add icons for new types and show temperature for sensors by default

--- web/composables/device_constants.ts
+++ web/composables/device_constants.ts
@@ -1,7 +1,8 @@
 export type TypeIconMapping = { _default: string, [type: string]: string | TypeIconMapping }
 
 export const MAIN_PROPERTIES: { type: string, property: string }[] = [
-    { type: "switch", property: "onoff" }
+    { type: "switch", property: "onoff" },
+    { type: "temperatureSensor", property: "temperature" }
 ]
 
 export const TYPE_ICONS: TypeIconMapping = {
@@ -12,8 +13,12 @@ export const TYPE_ICONS: TypeIconMapping = {
         "group": "mdi-lightbulb-group"
     },
     "remote": "mdi-remote",
+    "window": "mdi-curtains",
+    "shutter": "mdi-window-shutter",
+    "power": "mdi-power-socket",
     "motion": "mdi-motion-sensor",
     "presence": "mdi-location-enter",
+    "multilevel": "mdi-knob",
     "switch": "mdi-toggle-switch",
     "gateway": "mdi-lan",
     "controller": "mdi-cog-box",
