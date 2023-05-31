d58a4f395a71bf09f498cd885731f0180b834a03
Iwan Timmer <irtimmer@gmail.com>
2023-05-31 15:21:27 +0200

    influxdb: simple provider for writing history data

---
+++ providers/influxdb.ts
@@ -0,0 +1,31 @@
+import {InfluxDB, WriteApi, Point} from '@influxdata/influxdb-client'
+
+import { Logger } from '../shared/logger'
+
+type Config = {
+    url: string
+    token: string
+    organisation: string
+    bucket: string
+}
+
+export default class InfluxDBLogger implements Logger {
+    #writeApi: WriteApi
+
+    constructor(config: Config) {
+        const influxDB = new InfluxDB({
+            url: config.url,
+            token: config.token
+        })
+        this.#writeApi = influxDB.getWriteApi(config.organisation, config.bucket)
+    }
+
+    write(service: string, key: string, value: any): void {
+        const point = new Point('temperature')
+            .tag('service_id', service)
+            .tag('property', key)
+            .floatField('value', value)
+
+        this.#writeApi.writePoint(point)
+    }
+}
