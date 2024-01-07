5da6fd374e6aba0b8263e9394f15bd4d4437fa93
Iwan Timmer <irtimmer@gmail.com>
2024-01-07 18:14:49 +0100

    shared: add Retry helper class to complement Poll

--- shared/utils/poll.ts
+++ shared/utils/poll.ts
@@ -1,18 +1,18 @@
-type PollOptions = {
+type RetryOptions = {
     interval: number
     maxRetries: number
     retryInterval: number
 }
 
-export default class Poll {
-    #options: PollOptions
-    #fn: () => Promise<void>
+export class Retry {
+    #options: RetryOptions
     #timeout?: NodeJS.Timeout
     #canceled: boolean
     #retries: number
+    fn: () => Promise<void>
 
-    constructor(fn: () => Promise<void>, options?: Partial<PollOptions>) {
-        this.#fn = fn
+    constructor(fn: () => Promise<void>, options?: Partial<RetryOptions>) {
+        this.fn = fn
         this.#canceled = false
         this.#retries = 0
         this.#options = {...{
@@ -21,29 +21,47 @@ export default class Poll {
             maxRetries: 7
         }, ...options}
 
-        this.#run()
+        this.run()
     }
 
-    updateOptions(options: Partial<PollOptions>) {
-        this.#options = {...this.#options, ...options}
+    succeeded() {
+        this.#retries = 0
     }
 
-    #run() {
-        this.#fn().then(() => {
-            this.#retries = 0
-        }).catch((e) => {
-            this.#retries = Math.min(this.#retries + 1, this.#options.maxRetries)
-            console.error(`Attempt ${this.#retries} of ${this.#options.maxRetries}, error:`, e)
-        }).finally(() => {
-            if (!this.#canceled) {
-                let interval = this.#retries > 0 ? 2 ** this.#retries * this.#options.retryInterval : this.#options.interval
-                this.#timeout = setTimeout(this.#run.bind(this), interval)
-            }
+    run() {
+        this.#retries = Math.min(this.#retries + 1, this.#options.maxRetries)
+        this.fn().catch(e => {
+            this.retry(e)
         })
+    }
+
+    retry(e?: any) {
+        console.error(`Attempt ${this.#retries} of ${this.#options.maxRetries}`, e)
+        this.schedule()
+    }
+
+    schedule() {
+        if (this.#canceled)
+            return
+
+        let interval = this.#retries > 0 ? 2 ** this.#retries * this.#options.retryInterval : this.#options.interval
+        this.#timeout = setTimeout(this.run.bind(this), interval)
     }
 
     cancel() {
         if (this.#timeout)
             clearTimeout(this.#timeout)
+
+        this.#canceled = true
+    }
+}
+
+export default class Poll extends Retry {
+    run() {
+        this.fn().catch(e => this.retry(e))
+        .then(() => {
+            this.succeeded()
+            this.schedule.bind(this)
+        })
     }
 }
