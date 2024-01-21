f83f929501cabdbc85c8036a9892a0c208290d11
Iwan Timmer <irtimmer@gmail.com>
2024-01-21 17:09:23 +0100

    web: add list view to overview page

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,8 +1,17 @@
 <template>
   <q-page padding>
-    <div class="q-pa-md row items-start">
+    <q-toolbar class="text-primary">
+      <q-btn-toggle unelevated v-model="layout" toggle-color="primary" :options="[
+        {icon: 'mdi-view-comfy', value: 'cards'},
+        {icon: 'mdi-view-list', value: 'list'}
+      ]"/>
+    </q-toolbar>
+    <div v-if="layout == 'cards'" class="row items-start">
       <DeviceButton class="col-xs-6 col-sm-3 col-md-2 col-lg-1" v-for="[key, device] in store.devices.entries()" :device="device" :key="key" />
     </div>
+    <q-list v-else-if="layout == 'list'">
+      <DeviceItem v-for="[key, device] in store.devices.entries()" :device="device" :key="key" />
+    </q-list>
   </q-page>
 </template>
 
@@ -11,4 +20,6 @@ import { useStore } from '~~/stores/devices'
 
 const store = useStore()
 store.init()
+
+const layout = ref('cards')
 </script>
