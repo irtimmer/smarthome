4db9399a9b6213b545c1e6eb67f5f3f0a6a353ff
Iwan Timmer <irtimmer@gmail.com>
2024-02-19 21:20:47 +0100

    heatbooster: initial HeatBooster provider

---
+++ providers/heatbooster.ts
@@ -0,0 +1,45 @@
+import Provider from "../shared/provider";
+import Service from "../shared/service";
+import Poll from "../shared/utils/poll";
+
+import { HEATBOOSTER_PROPERTIES } from "./heatbooster_constants";
+
+type HeatBoosterConfig = {
+    url: string
+}
+
+export default class HeatBoosterProvider extends Provider<HeatBoosterService> {
+    constructor(id: string, config: HeatBoosterConfig) {
+        super(id)
+
+        const service = this.registerService(new HeatBoosterService(this, config))
+        const poll = new Poll(async () => service.update(), {
+            interval: 30
+        })
+        this.registerTask("poll", poll)
+    }
+}
+
+class HeatBoosterService extends Service<HeatBoosterProvider> {
+    #config: HeatBoosterConfig
+
+    constructor(provider: HeatBoosterProvider, config: HeatBoosterConfig) {
+        super(provider, provider.id)
+        this.#config = config
+        this.name = "HeatBooster"
+
+        this.updateTypes(['temperatureSensor'])
+        this.registerIdentifier('provider', provider.id)
+        for (const [key, property] of Object.entries(HEATBOOSTER_PROPERTIES))
+            this.registerProperty(key, {...{
+                read_only: true
+            }, ...property.defenition})
+    }
+
+    async update() {
+        return fetch(`${this.#config.url}/getStatus`).then((data: any) => data.json()).then((data: any) => {
+            for (const [key, property] of Object.entries(HEATBOOSTER_PROPERTIES))
+                this.updateValue(key, property.parse(data))
+        })
+    }
+}
---
+++ providers/heatbooster_constants.ts
@@ -0,0 +1,50 @@
+import { Property } from "../shared/definitions";
+
+type HeatBoosterProperty = {
+    parse: (data: any) => any,
+    defenition: Omit<Property, 'read_only'>
+}
+
+export const HEATBOOSTER_PROPERTIES: { [type: string]: HeatBoosterProperty } = {
+    T_AMB: {
+        parse: (data: any) => data.T_AMB,
+        defenition: {
+            '@type': 'temperature',
+            type: 'number',
+            unit: "C",
+            label: 'Room Temperature'
+        }
+    },
+    T_IN: {
+        parse: (data: any) => data.T_IN,
+        defenition: {
+            type: 'number',
+            unit: "C",
+            label: 'Inlet Temperature'
+        }
+    },
+    T_OUT: {
+        parse: (data: any) => data.T_OUT,
+        defenition: {
+            type: 'number',
+            unit: "C",
+            label: 'Outlet Temperature'
+        }
+    },
+    T_DELTA: {
+        parse: (data: any) => data.T_DELTA,
+        defenition: {
+            type: 'number',
+            unit: "C",
+            label: 'Delta Temperature'
+        }
+    },
+    FAN_SPEED: {
+        parse: (data: any) => data.FAN_SPEED,
+        defenition: {
+            type: 'number',
+            unit: "%",
+            label: 'Fan Speed'
+        }
+    },
+}
