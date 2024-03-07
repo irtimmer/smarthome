6d5ba50e603cf95c24f297a8e9adbcecd6498581
Iwan Timmer <irtimmer@gmail.com>
2024-03-07 22:07:09 +0100

    web: format timestamps

--- web/components/PropertyValue.vue
+++ web/components/PropertyValue.vue
@@ -1,5 +1,6 @@
 <template>
-  <span v-if="property.type == 'number'">{{ value }}<span v-if="property.unit"> {{ property.unit }}</span></span>
+  <span v-if="property.type == 'number' && property.logical_type == 'timestamp'">{{ new Date(value * 1000).toLocaleString() }}</span>
+  <span v-else-if="property.type == 'number'">{{ value }}<span v-if="property.unit"> {{ property.unit }}</span></span>
   <span v-else>{{ value }}</span>
 </template>
 
