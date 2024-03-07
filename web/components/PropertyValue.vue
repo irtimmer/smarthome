75fcc15634641d1c6dc8da9880d1ac0791ab1614
Iwan Timmer <irtimmer@gmail.com>
2024-03-07 22:07:31 +0100

    web: format float numbers

--- web/components/PropertyValue.vue
+++ web/components/PropertyValue.vue
@@ -1,14 +1,26 @@
 <template>
   <span v-if="property.type == 'number' && property.logical_type == 'timestamp'">{{ new Date(value * 1000).toLocaleString() }}</span>
-  <span v-else-if="property.type == 'number'">{{ value }}<span v-if="property.unit"> {{ property.unit }}</span></span>
+  <span v-else-if="property.type == 'number'">{{ numberValue }}<span v-if="property.unit"> {{ property.unit }}</span></span>
   <span v-else>{{ value }}</span>
 </template>
 
 <script setup lang="ts">
 import { type Property } from '~~/stores/devices';
 
-defineProps<{
+const props = defineProps<{
   value: any,
   property: Property
 }>()
+
+const numberValue = computed(() => {
+  const value = Number(props.value)
+  if (Number.isNaN(value))
+    return props.value
+  else if (Number.isInteger(value))
+    return value
+  else if (value < 1 && value > -1)
+    return value.toPrecision(2)
+  else
+    return value.toFixed(1)
+})
 </script>
