83d53df2ba1d7dc39e99212c3e8b94507fb2d7d9
Iwan Timmer <irtimmer@gmail.com>
2024-03-07 22:05:43 +0100

    web: make toggles more dense

--- web/components/PropertyInput.vue
+++ web/components/PropertyInput.vue
@@ -1,7 +1,7 @@
 <template>
   <PropertyValue v-if="property.read_only" :value="modelValue" :property="property" />
   <q-input v-else-if="property.type == 'number'" :modelValue="modelValue" borderless :min="property.min" :max="property.max" type="number" dense />
-  <q-toggle v-else-if="property.type == 'boolean'" :modelValue="modelValue" @update:modelValue="$emit('update:modelValue', $event)"></q-toggle>
+  <q-toggle dense v-else-if="property.type == 'boolean'" :modelValue="modelValue" @update:modelValue="$emit('update:modelValue', $event)"></q-toggle>
   <span v-else-if="property.type == 'string'">
     <PropertyValue :value="modelValue" :property="property" />
     <q-popup-edit :modelValue="modelValue" @update:modelValue="$emit('update:modelValue', $event)" :title="property.label" auto-save v-slot="scope">
