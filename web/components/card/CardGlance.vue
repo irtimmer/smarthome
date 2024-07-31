194ab91805277ae889ef1a58043c4844ecf3e8c7
Iwan Timmer <irtimmer@gmail.com>
2024-07-31 21:46:59 +0200

    web: keep order of entities in layouts

--- web/components/card/CardGlance.vue
+++ web/components/card/CardGlance.vue
@@ -14,13 +14,8 @@ const props = defineProps<{
 const store = useStore()
 
 const entities = computed(() => {
-  let entities = []
-  for (const [_, device] of store.devices.entries()) {
-    for (const entity of props.config.entities) {
-      if (device.identifiers.includes(entity))
-        entities.push(device)
-    }
-  }
+  const devices = Array.from(store.devices.values())
+  let entities = props.config.entities.map((entity: string) => devices.find(device => device.identifiers.includes(entity))).filter((x: any) => x)
   return entities
 })
 </script>
